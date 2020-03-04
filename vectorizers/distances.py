bf3e1cc8c6093c535ad14f127fcce2ec951fe2fc
Leland McInnes <leland.mcinnes@gmail.com>
2020-03-04 16:17:36 -0500

    Implement hellinger

--- vectorizers/distances.py
+++ vectorizers/distances.py
@@ -3,7 +3,22 @@ import numpy as np
 
 @numba.njit()
 def hellinger(x, y):
-    pass
+    result = 0.0
+    l1_norm_x = 0.0
+    l1_norm_y = 0.0
+    dim = x.shape[0]
+
+    for i in range(dim):
+        result += np.sqrt(x[i] * y[i])
+        l1_norm_x += x[i]
+        l1_norm_y += y[i]
+
+    if l1_norm_x == 0 and l1_norm_y == 0:
+        return 0.0
+    elif l1_norm_x == 0 or l1_norm_y == 0:
+        return 1.0
+    else:
+        return np.sqrt(1 - result / np.sqrt(l1_norm_x * l1_norm_y))
 
 
 @numba.njit()
@@ -29,3 +44,24 @@ def jensen_shannon_divergence(x, y):
 @numba.njit()
 def symmetric_kl_divergence(x, y):
     pass
+
+
+@numba.njit()
+def sparse_hellinger(ind1, data1, ind2, data2):
+    pass
+
+
+@numba.njit()
+def sparse_total_variation(ind1, data1, ind2, data2):
+    pass
+
+
+@numba.njit()
+def sparse_jensen_shannon_divergence(ind1, data1, ind2, data2):
+    pass
+
+
+@numba.njit()
+def sparse_symmetric_kl_divergence(ind1, data1, ind2, data2):
+    pass
+
