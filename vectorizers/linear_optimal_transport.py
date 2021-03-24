103ea596de9551f6aefd60ac69b8ad30f28985c2
Leland McInnes <leland.mcinnes@gmail.com>
2021-03-23 22:51:42 -0400

    Black the code

--- vectorizers/linear_optimal_transport.py
+++ vectorizers/linear_optimal_transport.py
@@ -16,6 +16,7 @@ from sklearn.preprocessing import normalize
 import os
 import tempfile
 
+
 @numba.njit(nogil=True, fastmath=True)
 def project_to_sphere_tangent_space(euclidean_vectors, sphere_basepoints):
     result = np.zeros_like(euclidean_vectors)
@@ -26,12 +27,14 @@ def project_to_sphere_tangent_space(euclidean_vectors, sphere_basepoints):
 
     return result
 
+
 @numba.njit(nogil=True, fastmath=True)
 def tangent_vectors_scales(reference_vectors, image_vectors):
     result = np.zeros((reference_vectors.shape[0], 1), dtype=np.float32)
     for i in range(result.shape[0]):
         result[i, 0] = cosine(reference_vectors[i], image_vectors[i])
     return result
+
 
 @numba.njit(nogil=True)
 def get_transport_plan(flow, graph):
@@ -46,12 +49,11 @@ def get_transport_plan(flow, graph):
 
     return result
 
+
 @numba.njit()
 def transport_plan(p, q, cost, max_iter=100000):
     node_arc_data, spanning_tree, graph = allocate_graph_structures(
-        p.shape[0],
-        q.shape[0],
-        False,
+        p.shape[0], q.shape[0], False,
     )
     initialize_supply(p, -q, graph, node_arc_data.supply)
     initialize_cost(cost, graph, node_arc_data.cost)
@@ -61,12 +63,7 @@ def transport_plan(p, q, cost, max_iter=100000):
         raise ValueError(
             "Optimal transport inputs must be valid probability distributions."
         )
-    solve_status = network_simplex_core(
-        node_arc_data,
-        spanning_tree,
-        graph,
-        max_iter,
-    )
+    solve_status = network_simplex_core(node_arc_data, spanning_tree, graph, max_iter,)
     if solve_status == ProblemStatus.INFEASIBLE:
         raise ValueError(
             "Optimal transport problem was INFEASIBLE. Please check " "inputs."
@@ -78,6 +75,7 @@ def transport_plan(p, q, cost, max_iter=100000):
     result = get_transport_plan(node_arc_data.flow, graph)
 
     return result
+
 
 @numba.njit(nogil=True, parallel=True)
 def chunked_pairwise_distance(data1, data2, dist=cosine, chunk_size=4):
@@ -97,6 +95,7 @@ def chunked_pairwise_distance(data1, data2, dist=cosine, chunk_size=4):
 
     return result
 
+
 # !! In place modification for efficiency
 @numba.njit(nogil=True, fastmath=True)
 def l2_normalize(vectors):
@@ -115,15 +114,15 @@ def l2_normalize(vectors):
 
 @numba.njit(nogil=True, parallel=True)
 def lot_vectors_sparse_internal(
-        indptr,
-        indices,
-        data,
-        sample_vectors,
-        reference_vectors,
-        reference_distribution,
-        metric=cosine,
-        max_distribution_size=256,
-        chunk_size=256,
+    indptr,
+    indices,
+    data,
+    sample_vectors,
+    reference_vectors,
+    reference_distribution,
+    metric=cosine,
+    max_distribution_size=256,
+    chunk_size=256,
 ):
     n_rows = indptr.shape[0] - 1
     result = np.zeros((n_rows, reference_vectors.size), dtype=np.float64)
@@ -132,8 +131,8 @@ def lot_vectors_sparse_internal(
         chunk_start = n * chunk_size
         chunk_end = min(chunk_start + chunk_size, n_rows)
         for i in range(chunk_start, chunk_end):
-            row_indices = indices[indptr[i]:indptr[i+1]]
-            row_distribution = data[indptr[i]:indptr[i+1]].astype(np.float64)
+            row_indices = indices[indptr[i] : indptr[i + 1]]
+            row_distribution = data[indptr[i] : indptr[i + 1]].astype(np.float64)
 
             if row_indices.shape[0] > max_distribution_size:
                 best_indices = np.argsort(-row_distribution)[:max_distribution_size]
@@ -148,12 +147,20 @@ def lot_vectors_sparse_internal(
             row_vectors = sample_vectors[row_indices].astype(np.float64)
 
             if row_vectors.shape[0] > reference_vectors.shape[0]:
-                cost = chunked_pairwise_distance(row_vectors, reference_vectors, dist=metric)
+                cost = chunked_pairwise_distance(
+                    row_vectors, reference_vectors, dist=metric
+                )
             else:
-                cost = chunked_pairwise_distance(reference_vectors, row_vectors, dist=metric).T
+                cost = chunked_pairwise_distance(
+                    reference_vectors, row_vectors, dist=metric
+                ).T
 
-            current_transport_plan = transport_plan(row_distribution, reference_distribution, cost)
-            transport_images = (current_transport_plan * (1.0 / reference_distribution)).T @ row_vectors
+            current_transport_plan = transport_plan(
+                row_distribution, reference_distribution, cost
+            )
+            transport_images = (
+                current_transport_plan * (1.0 / reference_distribution)
+            ).T @ row_vectors
 
             if metric == cosine:
                 l2_normalize(transport_images)
@@ -161,7 +168,9 @@ def lot_vectors_sparse_internal(
             transport_vectors = transport_images - reference_vectors
 
             if metric == cosine:
-                tangent_vectors = project_to_sphere_tangent_space(transport_vectors, reference_vectors)
+                tangent_vectors = project_to_sphere_tangent_space(
+                    transport_vectors, reference_vectors
+                )
                 l2_normalize(tangent_vectors)
                 scaling = tangent_vectors_scales(transport_images, reference_vectors)
                 transport_vectors = tangent_vectors * scaling
@@ -173,13 +182,13 @@ def lot_vectors_sparse_internal(
 
 @numba.njit(nogil=True, parallel=True)
 def lot_vectors_dense_internal(
-        sample_vectors,
-        sample_distributions,
-        reference_vectors,
-        reference_distribution,
-        metric=cosine,
-        max_distribution_size=256,
-        chunk_size=256,
+    sample_vectors,
+    sample_distributions,
+    reference_vectors,
+    reference_distribution,
+    metric=cosine,
+    max_distribution_size=256,
+    chunk_size=256,
 ):
     n_rows = len(sample_vectors)
     result = np.zeros((n_rows, reference_vectors.size), dtype=np.float64)
@@ -202,12 +211,20 @@ def lot_vectors_dense_internal(
                 row_distribution /= row_sum
 
             if row_vectors.shape[0] > reference_vectors.shape[0]:
-                cost = chunked_pairwise_distance(row_vectors, reference_vectors, dist=metric)
+                cost = chunked_pairwise_distance(
+                    row_vectors, reference_vectors, dist=metric
+                )
             else:
-                cost = chunked_pairwise_distance(reference_vectors, row_vectors, dist=metric).T
+                cost = chunked_pairwise_distance(
+                    reference_vectors, row_vectors, dist=metric
+                ).T
 
-            current_transport_plan = transport_plan(row_distribution, reference_distribution, cost)
-            transport_images = (current_transport_plan * (1.0 / reference_distribution)).T @ row_vectors
+            current_transport_plan = transport_plan(
+                row_distribution, reference_distribution, cost
+            )
+            transport_images = (
+                current_transport_plan * (1.0 / reference_distribution)
+            ).T @ row_vectors
 
             if metric == cosine:
                 l2_normalize(transport_images)
@@ -215,7 +232,9 @@ def lot_vectors_dense_internal(
             transport_vectors = transport_images - reference_vectors
 
             if metric == cosine:
-                tangent_vectors = project_to_sphere_tangent_space(transport_vectors, reference_vectors)
+                tangent_vectors = project_to_sphere_tangent_space(
+                    transport_vectors, reference_vectors
+                )
                 l2_normalize(tangent_vectors)
                 scaling = tangent_vectors_scales(transport_images, reference_vectors)
                 transport_vectors = tangent_vectors * scaling
@@ -226,15 +245,15 @@ def lot_vectors_dense_internal(
 
 
 def lot_vectors_sparse(
-        sample_vectors,
-        weight_matrix,
-        reference_vectors,
-        reference_distribution,
-        n_components=150,
-        metric=cosine,
-        random_state=None,
-        max_distribution_size=256,
-        block_size=16384
+    sample_vectors,
+    weight_matrix,
+    reference_vectors,
+    reference_distribution,
+    n_components=150,
+    metric=cosine,
+    random_state=None,
+    max_distribution_size=256,
+    block_size=16384,
 ):
     weight_matrix = weight_matrix.tocsr().astype(np.float64)
     weight_matrix = normalize(weight_matrix, norm="l1")
@@ -258,7 +277,10 @@ def lot_vectors_sparse(
             chunk_size=chunk_size,
         )
         u, singular_values, v = randomized_svd(
-            lot_vectors, n_components=n_components, n_iter=10, random_state=random_state,
+            lot_vectors,
+            n_components=n_components,
+            n_iter=10,
+            random_state=random_state,
         )
         result, components = svd_flip(u, v)
 
@@ -272,7 +294,7 @@ def lot_vectors_sparse(
         memmap_filename,
         mode="w+",
         shape=(n_rows, reference_vectors.size),
-        dtype = np.float32,
+        dtype=np.float32,
     )
 
     for i in range(n_blocks):
@@ -291,12 +313,17 @@ def lot_vectors_sparse(
         )
 
         if singular_values is not None:
-            block_to_learn = np.vstack((singular_values.reshape(-1, 1) * components, block))
+            block_to_learn = np.vstack(
+                (singular_values.reshape(-1, 1) * components, block)
+            )
         else:
             block_to_learn = block
 
         u, singular_values, v = randomized_svd(
-            block_to_learn, n_components=n_components, n_iter=10, random_state=random_state,
+            block_to_learn,
+            n_components=n_components,
+            n_iter=10,
+            random_state=random_state,
         )
         u, components = svd_flip(u, v)
         saved_blocks[block_start:block_end] = block
@@ -307,7 +334,7 @@ def lot_vectors_sparse(
         memmap_filename,
         mode="r",
         shape=(n_rows, reference_vectors.size),
-        dtype = np.float32,
+        dtype=np.float32,
     )
     result = saved_blocks @ components.T
     del saved_blocks
@@ -317,15 +344,15 @@ def lot_vectors_sparse(
 
 
 def lot_vectors_dense(
-        sample_vectors,
-        sample_distributions,
-        reference_vectors,
-        reference_distribution,
-        n_components=150,
-        metric=cosine,
-        random_state=None,
-        max_distribution_size=256,
-        block_size=16384
+    sample_vectors,
+    sample_distributions,
+    reference_vectors,
+    reference_distribution,
+    n_components=150,
+    metric=cosine,
+    random_state=None,
+    max_distribution_size=256,
+    block_size=16384,
 ):
     if metric == cosine:
         sample_vectors = normalize(sample_vectors, norm="l2")
@@ -345,7 +372,10 @@ def lot_vectors_dense(
             chunk_size=chunk_size,
         )
         u, singular_values, v = randomized_svd(
-            lot_vectors, n_components=n_components, n_iter=10, random_state=random_state,
+            lot_vectors,
+            n_components=n_components,
+            n_iter=10,
+            random_state=random_state,
         )
         result, components = svd_flip(u, v)
 
@@ -359,7 +389,7 @@ def lot_vectors_dense(
         memmap_filename,
         mode="w+",
         shape=(n_rows, reference_vectors.size),
-        dtype = np.float32,
+        dtype=np.float32,
     )
 
     for i in range(n_blocks):
@@ -376,12 +406,17 @@ def lot_vectors_dense(
         )
 
         if singular_values is not None:
-            block_to_learn = np.vstack((singular_values.reshape(-1, 1) * components, block))
+            block_to_learn = np.vstack(
+                (singular_values.reshape(-1, 1) * components, block)
+            )
         else:
             block_to_learn = block
 
         u, singular_values, v = randomized_svd(
-            block_to_learn, n_components=n_components, n_iter=10, random_state=random_state,
+            block_to_learn,
+            n_components=n_components,
+            n_iter=10,
+            random_state=random_state,
         )
         u, components = svd_flip(u, v)
         saved_blocks[block_start:block_end] = block
@@ -392,7 +427,7 @@ def lot_vectors_dense(
         memmap_filename,
         mode="r",
         shape=(n_rows, reference_vectors.size),
-        dtype = np.float32,
+        dtype=np.float32,
     )
     result = saved_blocks @ components.T
     del saved_blocks
