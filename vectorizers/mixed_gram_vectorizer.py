078248912fc5ecfcce735ade46535dfb349b94db
Leland McInnes <leland.mcinnes@gmail.com>
2021-12-14 22:41:32 -0500

    BytePairEncodingVectorizer core

--- vectorizers/mixed_gram_vectorizer.py
+++ vectorizers/mixed_gram_vectorizer.py
@@ -132,11 +132,242 @@ def counts_to_csr_data(count_dict, column_dict):
 
 
 @numba.njit(nogil=True)
-def byte_pair_based_encode(string, dictionary, hash_function=identity_hash, max_size=1<<20):
-    current_size = len(dictionary)
-    string_array = unicode_string_to_int_array(string)
+def count_pairs(char_list):
+    result = {}
+    for array in char_list:
+        for i in range(array.shape[0] - 1):
+            pair = (array[i], array[i + 1])
+            if pair in result:
+                result[pair] += 1
+            else:
+                result[pair] = 1
+    return result
 
-    pass
+
+@numba.njit(inline="always", nogil=True)
+def pair_length(pair, pair_lengths, max_char_code):
+    left_length = 1 if pair[0] <= max_char_code else pair_lengths[pair[0]]
+    right_length = 1 if pair[1] <= max_char_code else pair_lengths[pair[1]]
+    return left_length + right_length
+
+
+@numba.njit(
+    nogil=True,
+    locals=dict(
+        i=numba.uint32,
+        skip_char=numba.boolean,
+        len_char_list=numba.uint32,
+        last_char_added=numba.int64,
+    )
+)
+def contract_and_count_pairs(char_list, pair_to_contract, pair_counts, new_code=-1):
+    skip_char = False
+    len_char_list = len(char_list)
+    last_char_added = -1
+    new_char_list = np.zeros(len_char_list, dtype=np.int64)
+    new_char_index = 0
+
+    for i in range(len_char_list - 1):
+        if skip_char:
+            skip_char = False
+            continue
+
+        if char_list[i] == pair_to_contract[0] and char_list[i + 1] == pair_to_contract[1]:
+            if i > 0:
+                prior_pair = (last_char_added, char_list[i])
+                if prior_pair in pair_counts:
+                    pair_counts[prior_pair] -= 1
+                new_prior_pair = (last_char_added, new_code)
+                if new_prior_pair in pair_counts:
+                    pair_counts[new_prior_pair] += 1
+                else:
+                    pair_counts[new_prior_pair] = 1
+
+            if i < len_char_list - 2:
+                next_pair = (char_list[i + 1], char_list[i + 2])
+                if next_pair in pair_counts:
+                    pair_counts[next_pair] -= 1
+                new_next_pair = (new_code, char_list[i + 2])
+                if new_next_pair in pair_counts:
+                    pair_counts[new_next_pair] += 1
+                else:
+                    pair_counts[new_next_pair] = 1
+
+            new_char_list[new_char_index] = new_code
+            last_char_added = new_code
+            skip_char = True
+        else:
+            new_char_list[new_char_index] = char_list[i]
+            last_char_added = char_list[i]
+
+        new_char_index += 1
+
+    if not skip_char:
+        new_char_list[new_char_index] = char_list[i + 1]
+        new_char_index += 1
+
+    return new_char_list[:new_char_index], pair_counts
+
+
+@numba.njit(nogil=True)
+def pruning_max_freq_pair(count_dict, code_lengths, max_char_code, min_count=1):
+    result = (-1, -1)
+    max_count = 0
+    best_length = 0
+    keys_to_kill = set([(-1, -1)])
+    for pair, count in count_dict.items():
+        if count > max_count:
+            result = pair
+            max_count = count
+            best_length = pair_length(pair, code_lengths, max_char_code)
+        elif count == max_count:
+            length = pair_length(pair, code_lengths, max_char_code)
+            if length > best_length or (length == best_length and pair > result):
+                result = pair
+                max_count = count
+                best_length = length
+        elif count <= min_count:
+            keys_to_kill.add(pair)
+
+    if len(keys_to_kill) > 0:
+        for key in keys_to_kill:
+            if key[0] >= 0:
+                count_dict.pop(key)
+
+    if max_count == 1:
+        return (-1, -1), 0
+
+    return result, max_count
+
+
+@numba.njit(inline="always", nogil=True)
+def to_unicode(code, tokens, max_char_code):
+    if code <= max_char_code:
+        return chr(code)
+    else:
+        return tokens[code - max_char_code - 1]
+
+
+@numba.njit(inline="always", nogil=True)
+def to_code_num(code, code_list, max_char_code):
+    if code <= max_char_code:
+        return [code]
+    else:
+        return code_list[code - max_char_code - 1]
+
+
+@numba.njit(inline="always", nogil=True)
+def pair_to_string(pair, tokens, max_char_code):
+    return to_unicode(pair[0], tokens, max_char_code) + to_unicode(pair[1], tokens, max_char_code)
+
+
+@numba.njit(inline="always", nogil=True)
+def pair_to_list(pair, code_list, max_char_code):
+    return to_code_num(pair[0], code_list, max_char_code) + to_code_num(pair[1], code_list, max_char_code)
+
+
+@numba.njit()
+def bpe_train(char_list, vocab_size=10000, min_count=1):
+    # Initialize compressed chars
+    compressed_chars = [np.empty(len(chars), dtype=np.int64) for chars in char_list]
+    max_char_code = 0
+    for i, chars in enumerate(char_list):
+        for j, c in enumerate(chars):
+            c_val = ord(c)
+            compressed_chars[i][j] = c_val
+            if c_val > max_char_code:
+                max_char_code = c_val
+
+    # Initialize coding, counts, and lengths
+    new_code = max_char_code + 1
+    pair_counts = count_pairs(compressed_chars)
+    current_min_count = np.max(np.array(list(pair_counts.values()))) // 2
+    code_lengths = {-1: 1}
+
+    # Initialize code words and lengths so numba gets the types right
+    pair_to_replace, count = pruning_max_freq_pair(
+        pair_counts, code_lengths, max_char_code, min_count=current_min_count
+    )
+    tokens = [chr(pair_to_replace[0]) + chr(pair_to_replace[1])]
+    code_list = [pair_to_replace]
+    code_lengths[new_code] = pair_length(pair_to_replace, code_lengths, max_char_code)
+
+    while len(tokens) < vocab_size:
+        for i, char_array in enumerate(compressed_chars):
+            compressed_chars[i], pair_counts = contract_and_count_pairs(
+                char_array, pair_to_replace, pair_counts, new_code
+            )
+
+        pair_counts.pop(pair_to_replace)
+        new_code += 1
+        pair_to_replace, count = pruning_max_freq_pair(
+            pair_counts, code_lengths, max_char_code, min_count=current_min_count
+        )
+
+        if current_min_count > min_count and count <= current_min_count:
+            current_min_count = max(current_min_count // 2, min_count)
+            pair_counts = count_pairs(compressed_chars)
+            pair_to_replace, count = pruning_max_freq_pair(
+                pair_counts, code_lengths, max_char_code,
+                min_count=current_min_count
+            )
+
+        if pair_to_replace[0] >= 0 and pair_counts[pair_to_replace] > 1:
+            tokens.append(pair_to_string(pair_to_replace, tokens, max_char_code))
+            code_list.append(pair_to_replace)
+            code_lengths[new_code] = pair_length(pair_to_replace, code_lengths, max_char_code)
+        else:
+            break
+
+    return tokens, code_list, compressed_chars, max_char_code
+
+
+@numba.njit()
+def contract_pair(char_list, pair_to_contract, new_code=-1):
+    skip_char = False
+    len_char_list = len(char_list)
+    new_char_list = np.zeros(len_char_list, dtype=np.int64)
+    new_char_index = 0
+
+    for i in range(len_char_list - 1):
+        if skip_char:
+            skip_char = False
+            continue
+
+        if char_list[i] == pair_to_contract[0] and char_list[i + 1] == pair_to_contract[1]:
+            new_char_list[new_char_index] = new_code
+            skip_char = True
+        else:
+            new_char_list[new_char_index] = char_list[i]
+
+        new_char_index += 1
+
+    if not skip_char:
+        new_char_list[new_char_index] = char_list[i + 1]
+        new_char_index += 1
+
+    return new_char_list[:new_char_index]
+
+
+@numba.njit(nogil=True)
+def bpe_encode(chars, code_list, max_char_code):
+    compressed_chars = np.empty(len(chars), dtype=np.int64)
+    for i, c in enumerate(chars):
+        compressed_chars[i] = ord(c)
+
+    new_code = max_char_code + 1
+    for code_pair in code_list:
+        compressed_chars = contract_pair(compressed_chars, code_pair, new_code=new_code)
+        new_code += 1
+
+    return compressed_chars
+
+def bpe_decode(code_array, tokens, max_char_code):
+    result = [
+        chr(c) if c <= max_char_code else tokens[c - max_char_code - 1]
+        for c in code_array
+    ]
+    return "".join(result)
 
 class LZCompressionVectorizer(BaseEstimator, TransformerMixin):
 
@@ -175,7 +406,7 @@ class LZCompressionVectorizer(BaseEstimator, TransformerMixin):
                 input_dict = numba.typed.Dict.empty(numba.types.unicode_type, numba.types.int64)
 
             if self.base_dictionary is not None:
-                for key, val in base_dictionary.items():
+                for key, val in self.base_dictionary.items():
                     input_dict[key] = val
 
             encoding_dict = lempel_ziv_based_encode(string, input_dict, hash_function, self.max_dict_size)
@@ -214,16 +445,16 @@ class LZCompressionVectorizer(BaseEstimator, TransformerMixin):
             if self.base_dictionary is None:
                 input_dict = {}
             else:
-                input_dict = base_dictionary.copy()
+                input_dict = self.base_dictionary.copy()
 
             encoding_dict = lempel_ziv_based_encode(string, input_dict, self.hash_function, self.max_size)
 
             for ngram in encoding_dict.keys():
                 if ngram in self.column_label_dictionary_:
                     col = self.column_label_dictionary_[ngram]
-
-                indices.append(col)
-                data.append(val)
+                    val = encoding_dict[ngram]
+                    indices.append(col)
+                    data.append(val)
 
             indptr.append(indptr[-1] + len(encoding_dict))
 
@@ -245,4 +476,97 @@ class LZCompressionVectorizer(BaseEstimator, TransformerMixin):
 
         return result
 
+
+class BytePairEncodingVectorizer(BaseEstimator, TransformerMixin):
+
+    def __init__(self, max_vocab_size=10000, min_token_occurrence=1, return_type="matrix"):
+
+        self.max_vocab_size = max_vocab_size
+        self.min_token_occurence = min_token_occurrence
+        self.return_type = return_type
+
+
+    def fit_transform(self, X, y=None, **fit_params):
+        if self.return_type not in ("matrix", "tokens", "sequences"):
+            raise ValueError(f"return_type must be one of 'matrix' 'tokens', or 'sequences', not {self.return_type}")
+        if not type(self.max_vocab_size) == int or self.max_vocab_size <= 0:
+            raise ValueError(f"max_vocab_size should be a non-zero positive integer not {self.max_vocab_size}")
+        if not type(self.min_token_occurence) == int or self.min_token_occurence <= 0:
+            raise ValueError(f"min_token_occurence should be a non-zero positive integer not {self.min_token_occurence}")
+
+        (
+            self.tokens_,
+            self.code_list_,
+            encodings,
+            self.max_char_code_,
+        ) = bpe_train(X, vocab_size=self.max_vocab_size, min_count=self.min_token_occurence)
+
+        if self.return_type == "sequences":
+            return encodings
+        elif self.return_type == "tokens":
+            result = []
+            for row in encodings:
+                result.append(
+                    [
+                        chr(x) if x <= self.max_char_code_ else self.tokens_[x - self.max_char_code_ - 1]
+                        for x in row
+                    ]
+                )
+            return result
+        elif self.return_type == "matrix":
+            unique_codes = np.unique(np.hstack(encodings))
+            self.column_label_dictionary_ = dict(zip(unique_codes, np.arange(unique_codes.shape[0])))
+            indices = []
+            data = []
+            indptr = [0]
+
+            for row in encodings:
+                indices.extend([self.column_label_dictionary_[x] for x in row])
+                data.extend([1 for i in range(row.shape[0])])
+                indptr.append(len(indices))
+
+            result = scipy.sparse.csr_matrix((data, indices, indptr), dtype=np.float32)
+            result.sum_duplicates()
+
+            return result
+        else:
+            raise ValueError(f"return_type must be one of 'matrix' 'tokens', or 'sequences', not {self.return_type}")
+
+    def fit(self, X, y=None, **fit_params):
+        self.fit_transform(X, y, **fit_params)
+        return self
+
+    def transform(self, X, y=None):
+        check_is_fitted(self, ["tokens_", "code_list_", "max_char_code_"])
+
+        encodings = [bpe_encode(string) for string in X]
+
+        if self.return_type == "sequences":
+            return encodings
+        elif self.return_type == "tokens":
+            result = []
+            for row in encodings:
+                result.append(
+                    [
+                        chr(x) if x <= self.max_char_code_ else self.tokens_[x - self.max_char_code_ - 1]
+                        for x in row
+                    ]
+                )
+            return result
+        elif self.return_type == "matrix":
+            indices = []
+            data = []
+            indptr = [0]
+
+            for row in encodings:
+                indices.extend([self.column_label_dictionary_[x] for x in row])
+                data.extend([1 for i in range(row.shape[0])])
+                indptr.append(len(indices))
+
+            result = scipy.sparse.csr_matrix((data, indices, indptr), dtype=np.float32)
+            result.sum_duplicates()
+
+            return result
+        else:
+            raise ValueError(f"return_type must be one of 'matrix' 'tokens', or 'sequences', not {self.return_type}")
 
