9a637b1a3c8a7db2a1619b441ececa0668d88533
Colin Weir (PB) <colinoftheweirs@gmail.com>
2022-05-13 12:05:52 -0400

    default dict fix

--- vectorizers/ngram_token_cooccurence_vectorizer.py
+++ vectorizers/ngram_token_cooccurence_vectorizer.py
@@ -108,7 +108,7 @@ def numba_build_skip_grams(
     for d_i, seq in enumerate(token_sequences):
         for w_i in range(ngram_size - 1, len(seq)):
             ngram = array_to_tuple(seq[w_i - ngram_size + 1 : w_i + 1])
-            target_gram_ind = np.int32(ngram_dictionary.get(ngram))
+            target_gram_ind = np.int32(ngram_dictionary.get(ngram, default=-1))
             if target_gram_ind >= 0:
                 target_gram_ind = np.int64(target_gram_ind)
                 windows = [
@@ -229,7 +229,7 @@ def numba_em_cooccurrence_iteration(
     for d_i, seq in enumerate(token_sequences):
         for w_i in range(ngram_size - 1, len(seq)):
             ngram = array_to_tuple(seq[w_i - ngram_size + 1 : w_i + 1])
-            target_gram_ind = np.int32(ngram_dictionary.get(ngram))
+            target_gram_ind = np.int32(ngram_dictionary.get(ngram, default=-1))
             if target_gram_ind >= 0:
                 windows = [
                     window_at_index(
